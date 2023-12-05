8235fd38703083c04bd804510e0b95ce27fc62ad
Christian <145691299+Fulache3@users.noreply.github.com>
2023-12-05 21:03:51 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function navBar_function(){
-    const navigationBar =  document.getElementsByClassName("right-nav-bar")[0];
-    
-    if(window.getComputedStyle(navigationBar).visibility==="hidden"){
-      navigationBar.style.visibility ='visible';
-    }else if(window.getComputedStyle(navigationBar).visibility==="visible"){
-      navigationBar.style.visibility='hidden';
-    }
-}
